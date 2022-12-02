9b6929a4398daa2437d59800f13bdf7f7064d509
sarpsenturk <108862920+sarpsenturk@users.noreply.github.com>
2022-12-02 23:09:04 +0300

    Update EnableGoogleTest.cmake

--- cmake/EnableGoogleTest.cmake
+++ cmake/EnableGoogleTest.cmake
@@ -1,16 +1,12 @@
-find_package(GTest)
-if (GTest_Found)
-    message("-- GTest found")
-else ()
-    set(GOOGLETEST_COMMIT_ID "e68764c147ea0dac1e8811925c531d937396878e" CACHE STRING "GoogleTest commit ID")
-    message(STATUS "Using FetchContent to get GTest with commit ${GOOGLETEST_COMMIT_ID}.")
-    include(FetchContent)
-    FetchContent_Declare(
-            googletest
-            URL https://github.com/google/googletest/archive/${GOOGLETEST_COMMIT_ID}.zip
-    )
-    # For Windows: Prevent overriding the parent project's compiler/linker settings
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-    FetchContent_MakeAvailable(googletest)
-endif ()
-
+set(GOOGLETEST_GIT_TAG "release-1.12.1" CACHE STRING "GoogleTest tag to use")
+include(FetchContent)
+FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG ${GOOGLETEST_GIT_TAG}
+        FIND_PACKAGE_ARGS NAMES GTest
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
+include(GoogleTest)
