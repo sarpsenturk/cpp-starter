354725f5bc62a8e18b7afdce1cab8df6546024c1
sarpsenturk <sarpsenturk38@gmail.com>
2022-12-04 19:48:15 +0300

    Remove starter source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
-#include "cpp-starter/cpp-starter.h"
-
 #include <fmt/core.h>
 
 int main(int argc, const char* argv[])
 {
     fmt::print("Hello world\n");
-    return cpp_starter::return_ok();
+    return 0;
 }
